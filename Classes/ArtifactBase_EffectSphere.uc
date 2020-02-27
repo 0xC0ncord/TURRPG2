0xC0ncord/TURRPG2

commit 36a333c26aa9e64dd5847abdace2e87a0f668ba8

Fix sphere artifacts not working properly with instant effects

Classes/ArtifactBase_EffectSphere.uc
@@ -38,6 +38,9 @@ state Activated
 
         CreateEffects();
 
+        if(class<RPGInstantEffect>(EffectClass) != None)
+            return;
+
         for(i = 0; i < Pawns.Length; i++)
         {
             if(Pawns[i] == None || Pawns[i].Health <= 0 || VSize(Pawns[i].Location - CoreLocation) > Radius)
@@ -84,16 +87,19 @@ function CreateEffects()
         NextC = C.NextController;
         if(C.Pawn != None && VSize(C.Pawn.Location - CoreLocation) <= Radius)
         {
-            if(class'Util'.static.InArray(C.Pawn, Pawns) == -1 && CanApplyEffectOn(C.Pawn))
+            if(CanApplyEffectOn(C.Pawn))
             {
+                if(class<RPGInstantEffect>(EffectClass) == None && class'Util'.static.InArray(C.Pawn, Pawns) == -1)
+                    continue;
+                else
+                    Pawns[Pawns.Length] = C.Pawn;
+
                 Effect = EffectClass.static.Create(C.Pawn, Instigator.Controller, EstimatedRunTime, EstimatedRunTime);
                 if(Effect != None)
                 {
                     ModifyEffect(Effect);
                     Effect.Start();
                 }
-
-                Pawns[Pawns.Length] = C.Pawn;
             }
         }
         C = NextC;

commit 2666a5b7e81d75dd6d292cf98ba5c565ff12f01c

Fix regression in sphere artifacts that caused them to become inoperable if not using instant effects
Regression was introduced in commit 36a333c

Classes/ArtifactBase_EffectSphere.uc
@@ -89,8 +89,11 @@ function CreateEffects()
         {
             if(CanApplyEffectOn(C.Pawn))
             {
-                if(class<RPGInstantEffect>(EffectClass) == None && class'Util'.static.InArray(C.Pawn, Pawns) == -1)
+                if(class<RPGInstantEffect>(EffectClass) == None && class'Util'.static.InArray(C.Pawn, Pawns) != -1)
+                {
+                    C = NextC;
                     continue;
+                }
                 else
                     Pawns[Pawns.Length] = C.Pawn;
 