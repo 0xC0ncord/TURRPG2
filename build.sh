6b8aaea408f387252db97154ab25985f3a8d958b
0xC0ncord <me@concord.sh>
2021-12-24 17:15:02 -0500

    build: cleanups and improvements to build script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 CURRENT_DIR="$(pwd)"
 BASENAME="$(basename ${CURRENT_DIR})"
 SYSTEM_DIR="${CURRENT_DIR}/../System"
 UCC="${SYSTEM_DIR}/ucc.exe"
 
-if [[ -f ${BASENAME}.inc ]]; then
-    VERSION=$(sed 's/^#define __VERSION__ \(.*\)$/\1/p;d' ${BASENAME}.inc)
+if [[ -f "${BASENAME}.inc" ]]; then
+    VERSION="$(sed 's/^#define __VERSION__ \(.*\)$/\1/p;d' ${BASENAME}.inc)"
 else
     VERSION="UNKNOWN_VERSION"
 fi
 if [[ -d .git ]]; then
-    BUILD_INFO=$(git branch | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/;s/\s\+/_/g')-$(git rev-parse --short HEAD)$([ -z "$(git status --porcelain 2>/dev/null | tail -n1)" ] || echo '-dirty')
+    BUILD_INFO="$(git branch | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/;s/\s\+/_/g')-$(git rev-parse --short HEAD)$([ -z "$(git status --porcelain 2>/dev/null | tail -n1)" ] || echo '-dirty')"
     VERSION_STRING="${VERSION}-${BUILD_INFO}"
 else
     BUILD_INFO=""
@@ -27,9 +27,9 @@ PREPROCESSED=0
 EXIT_STATUS=0
 
 restore () {
-    if [[ NO_RESTORE -eq 0 ]]; then
-        rm -rf ${CURRENT_DIR}/Classes
-        mv -vf ${CURRENT_DIR}/.Classes ${CURRENT_DIR}/Classes
+    if [[ ${NO_RESTORE} -eq 0 ]]; then
+        rm -rf "${CURRENT_DIR}"/Classes
+        mv -vf "${CURRENT_DIR}"/.Classes "${CURRENT_DIR}"/Classes
     fi
 }
 
@@ -70,53 +70,50 @@ echo -e "\e[92mBuild Info....: ${BUILD_INFO}\e[0m"
 echo -e "\e[92mVersion String: ${VERSION_STRING}\e[0m"
 echo -e "\e[92mBuild Date....: ${BUILD_DATE}\e[0m"
 echo Preparing...
-if [[ -d ${CURRENT_DIR}/.Classes  ]]; then
+if [[ -d "${CURRENT_DIR}"/.Classes  ]]; then
     echo -e "\e[91m(!) Original Classes source tree appears to have not been restored.\e[0m"
     echo -e "\e[91m    If you think this is in error, run 'make clean' before invoking the build script again.\e[0m"
     echo -e "\e[93mContinuing in 5 seconds...\e[0m"
     sleep 5
-    SHA1SUM="$(find ${CURRENT_DIR}/.Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
+    SHA1SUM="$(find "${CURRENT_DIR}"/.Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
 else
-    SHA1SUM="$(find ${CURRENT_DIR}/Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
+    SHA1SUM="$(find "${CURRENT_DIR}"/Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
 fi
-SHA1SUM_OLD="$(cat ${CURRENT_DIR}/Classes.sha1sum 2>/dev/null)"
-if [[ -f ${CURRENT_DIR}/Classes.sha1sum && "${SHA1SUM_OLD}" = "${SHA1SUM}" ]]; then echo 'Source directory unchanged since last build. Nothing to do. Exiting.' && exit 1; fi
+SHA1SUM_OLD="$(cat "${CURRENT_DIR}"/Classes.sha1sum 2>/dev/null)"
+if [[ -f "${CURRENT_DIR}"/Classes.sha1sum && "${SHA1SUM_OLD}" = "${SHA1SUM}" ]]; then echo 'Source directory unchanged since last build. Nothing to do. Exiting.' && exit 1; fi
 if [[ ${NO_PREPROCESS} -eq 0 ]]; then
     echo Preprocessing...
-    if [[ -f ${CURRENT_DIR}/Classes/.preprocessed ]]; then
+    if [[ -f "${CURRENT_DIR}"/Classes/.preprocessed ]]; then
         echo -e "\e[91m(!) Classes source tree appears to have already been preprocessed. Skipping...\e[0m"
         echo -e "\e[91m    If you think this is in error, run 'make clean' before invoking the build script again.\e[0m"
     else
-        cp -r ${CURRENT_DIR}/Classes ${CURRENT_DIR}/.Classes
-        if [[ ${RELEASE_BUILD} -eq 0 ]]; then
-            CMDLINE="-D__DEBUG__"
+        cp -r "${CURRENT_DIR}"/Classes "${CURRENT_DIR}"/.Classes
+
+        OPTS=()
+        [[ ${RELEASE_BUILD} -eq 0 ]] && OPTS+=("-D__DEBUG__")
+        if [[ -f "${CURRENT_DIR}/${BASENAME}.inc" ]]; then
+            OPTS+=("--nostdinc")
+            OPTS+=("--include ${CURRENT_DIR}/${BASENAME}.inc")
         fi
-        if [[ -f ${CURRENT_DIR}/${BASENAME}.inc ]]; then
-            for FILE in ${CURRENT_DIR}/.Classes/*.uc; do
-                if [[ -n "${CMDLINE}" ]]; then
-                    gpp -C "${CMDLINE}" -D__VERSION__="${VERSION}" -D__BUILDINFO__="${BUILD_INFO}" -D__VERSIONSTRING__="${VERSION_STRING}" -D__BUILDDATE__="${BUILD_DATE}" --include ${CURRENT_DIR}/${BASENAME}.inc ${FILE} -o ${CURRENT_DIR}/Classes/$(basename ${FILE})
-                else
-                    gpp -C -D__VERSION__="${VERSION}" -D__BUILDINFO__="${BUILD_INFO}" -D__VERSIONSTRING__="${VERSION_STRING}" -D__BUILDDATE__="${BUILD_DATE}" --include ${CURRENT_DIR}/${BASENAME}.inc ${FILE} -o ${CURRENT_DIR}/Classes/$(basename ${FILE})
-                fi
-            done
-        else
-            for FILE in ${CURRENT_DIR}/.Classes/*.uc; do
-                if [[ -n "${CMDLINE}" ]]; then
-                    gpp -C "${CMDLINE}" -D__VERSION__="${VERSION}" -D__BUILDINFO__="${BUILD_INFO}" -D__VERSIONSTRING__="${VERSION_STRING}" -D__BUILDDATE__="${BUILD_DATE}" ${FILE} -o ${CURRENT_DIR}/Classes/$(basename ${FILE})
-                else
-                    gpp -C -D__VERSION__="${VERSION}" -D__BUILDINFO__="${BUILD_INFO}" -D__VERSIONSTRING__="${VERSION_STRING}" -D__BUILDDATE__="${BUILD_DATE}" ${FILE} -o ${CURRENT_DIR}/Classes/$(basename ${FILE})
-                fi
-            done
-        fi
-        touch ${CURRENT_DIR}/Classes/.preprocessed
+
+        for FILE in "${CURRENT_DIR}"/.Classes/*.uc; do
+            gpp -C ${OPTS[@]} \
+                -D__VERSION__="${VERSION}" \
+                -D__BUILDINFO__="${BUILD_INFO}" \
+                -D__VERSIONSTRING__="${VERSION_STRING}" \
+                -D__BUILDDATE__="${BUILD_DATE}" \
+                -o "${CURRENT_DIR}/Classes/$(basename ${FILE})" \
+                "${FILE}"
+        done
+        touch "${CURRENT_DIR}"/Classes/.preprocessed
     fi
 fi
 echo Compiling...
-if [[ -f ${SYSTEM_DIR}/${BASENAME}.u ]]; then mv -vf ${SYSTEM_DIR}/${BASENAME}.u ${SYSTEM_DIR}/.${BASENAME}.u.bak; fi
+[[ -f "${SYSTEM_DIR}/${BASENAME}.u" ]] && mv -vf "${SYSTEM_DIR}/${BASENAME}.u" "${SYSTEM_DIR}/.${BASENAME}.u.bak"
 {
     wine ${UCC} make ini=..\\${BASENAME}\\make.ini 2>/dev/null && echo ${SHA1SUM} > ${CURRENT_DIR}/Classes.sha1sum
 } || {
-    mv -vf ${SYSTEM_DIR}/.${BASENAME}.u.bak ${SYSTEM_DIR}/${BASENAME}.u
+    mv -vf "${SYSTEM_DIR}/.${BASENAME}.u.bak" "${SYSTEM_DIR}/${BASENAME}.u"
     EXIT_STATUS=1
 }
 restore
