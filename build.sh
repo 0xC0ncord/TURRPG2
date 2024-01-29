d980be580441f8e26ba668faf4cbbc3446e54ba9
0xC0ncord <me@concord.sh>
2024-01-28 20:03:21 -0500

    Massive rework to build script

--- build.sh
+++ build.sh
@@ -1,105 +1,166 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$(pwd)"
-BASENAME="$(basename "${CURRENT_DIR}")"
-SYSTEM_DIR="${CURRENT_DIR}/../System"
-UCC="${SYSTEM_DIR}/UCC.exe"
-if [[ "$(uname -r)" == *"WSL"* ]]; then
-    COMPILE="${UCC}"
-else
-    COMPILE="wine ${UCC}"
-fi
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
 
-if [[ -f "${BASENAME}.inc" ]]; then
-    VERSION="$(sed 's/^#define __VERSION__ \(.*\)$/\1/p;d' "${BASENAME}".inc)"
-else
-    VERSION="UNKNOWN_VERSION"
-fi
-if [[ -d .git ]]; then
-    BUILD_INFO="$(git branch | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/;s/\s\+/_/g')-$(git rev-parse --short HEAD)$([ -z "$(git status --porcelain 2>/dev/null | tail -n1)" ] || echo '-dirty')"
-    VERSION_STRING="${VERSION}-${BUILD_INFO}"
-else
-    BUILD_INFO=""
-    VERSION_STRING="${VERSION}"
-fi
-BUILD_DATE="$(date +"%a %d %b %Y %H:%M:%S %Z")"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
+PACKAGE="$(basename "${BASE_DIR}")"
 
-NO_RESTORE=0
-NO_PREPROCESS=0
-RELEASE_BUILD=0
+usage() {
+    cat <<_EOF_
+Usage: $(basename "${BASH_SOURCE[0]}") [-h|--help] [-v|--verbose] [-n|--no-restore] [-s|--skip-preprocess] [-r|--release]
 
-PREPROCESSED=0
-EXIT_STATUS=0
+Build the package and all.
 
-restore () {
-    if [[ ${NO_RESTORE} -eq 0 ]]; then
-        rm -rf "${CURRENT_DIR}"/Classes
-        mv -vf "${CURRENT_DIR}"/.Classes "${CURRENT_DIR}"/Classes
+Available options:
+    -h, --help              Print this message
+    -v, --verbose           Show extra output
+    -n, --no-restore        Don't restore the original Classes source tree when finishing
+    -s, --skip-preprocess   Skip the preprocessing phase entirely
+    -r, --release           Compile with the __DEBUG__ macro undefined (for release builds)
+_EOF_
+    exit 0
+}
+
+cleanup() {
+    trap - SIGINT SIGTERM ERR EXIT
+}
+
+setup_colors() {
+    if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+        NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+    else
+        NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
     fi
 }
 
-for ARG in "$@"; do
-    case $ARG in
-        "-h" | "--help")
-            echo "Usage: build.sh [options]"
-            echo
-            echo " -n, --no-restore      : don't restore original Classes source tree when finishing"
-            echo " -s, --skip-preprocess : skip the preprocessing phase entirely"
-            echo " -r, --release         : compile with the __DEBUG__ macro undefined (for release builds)"
-            exit 0
-            ;;
-        "-n" | "--no-restore")
-            NO_RESTORE=1
-            ;;
-        "-s" | "--skip-preprocess")
-            NO_PREPROCESS=1
-            ;;
-        "-r" | "--release")
-            RELEASE_BUILD=1
-            ;;
-        *)
-            echo "Unrecognized option \'$ARG\'"
-            exit 1
-            ;;
-    esac
-done
+msg() {
+    echo -e "${@-}" >&2
+}
 
-if [[ -n "$(lsof +D "${CURRENT_DIR}"/Classes)" ]]; then echo 'Classes directory tree open in some program(s). Please close before continuing.' && exit 1; fi
-if [[ ${RELEASE_BUILD} -eq 0 ]]; then
-    echo -e "\e[92mBuild: DEBUG\e[0m"
-else
-    echo -e "\e[92mBuild: RELEASE\e[0m"
-fi
-echo -e "\e[92mVersion.......: ${VERSION}\e[0m"
-echo -e "\e[92mBuild Info....: ${BUILD_INFO}\e[0m"
-echo -e "\e[92mVersion String: ${VERSION_STRING}\e[0m"
-echo -e "\e[92mBuild Date....: ${BUILD_DATE}\e[0m"
-echo Preparing...
-if [[ -d "${CURRENT_DIR}"/.Classes  ]]; then
-    echo -e "\e[91m(!) Original Classes source tree appears to have not been restored.\e[0m"
-    echo -e "\e[91m    If you think this is in error, run 'make clean' before invoking the build script again.\e[0m"
-    echo -e "\e[93mContinuing in 5 seconds...\e[0m"
-    sleep 5
-    SHA1SUM="$(find "${CURRENT_DIR}"/.Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
-else
-    SHA1SUM="$(find "${CURRENT_DIR}"/Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
-fi
-SHA1SUM_OLD="$(cat "${CURRENT_DIR}"/Classes.sha1sum 2>/dev/null)"
-if [[ -f "${CURRENT_DIR}"/Classes.sha1sum && "${SHA1SUM_OLD}" = "${SHA1SUM}" ]]; then echo 'Source directory unchanged since last build. Nothing to do. Exiting.' && exit 1; fi
-if [[ ${NO_PREPROCESS} -eq 0 ]]; then
-    echo Preprocessing...
-    if [[ -f "${CURRENT_DIR}"/Classes/.preprocessed ]]; then
-        echo -e "\e[91m(!) Classes source tree appears to have already been preprocessed. Skipping...\e[0m"
-        echo -e "\e[91m    If you think this is in error, run 'make clean' before invoking the build script again.\e[0m"
+die() {
+    echo -e "${@-}" >&2
+    exit 1
+}
+
+parse_args() {
+    NO_RESTORE=0
+    NO_PREPROCESS=0
+    RELEASE_BUILD=0
+
+    while :; do
+        case "${1-}" in
+            "-h"|"--help")
+                usage
+                ;;
+            "-v"|"--verbose")
+                set -x
+                ;;
+            "-n"|"--no-restore")
+                NO_RESTORE=1
+                ;;
+            "-s"|"--skip-preprocess")
+                NO_PREPROCESS=1
+                ;;
+            "-r"|"--release")
+                RELEASE_BUILD=1
+                ;;
+            -?*)
+                die "Unknown option: ${1}"
+                ;;
+            *)
+                break
+                ;;
+        esac
+        shift
+    done
+
+    return 0
+}
+
+setup() {
+    msg "${GREEN}>>> ${NOFORMAT}Setting up the build environment..."
+
+    SYSTEM_DIR="${BASE_DIR}/../System"
+    local ucc="${SYSTEM_DIR}/UCC.exe"
+
+    if [[ "$(uname -r)" == *"WSL"* ]]; then
+        COMPILE="${ucc}"
     else
-        cp -r "${CURRENT_DIR}"/Classes "${CURRENT_DIR}"/.Classes
+        COMPILE="wine ${ucc}"
+    fi
 
-        OPTS=()
-        [[ ${RELEASE_BUILD} -eq 0 ]] && OPTS+=("-D__DEBUG__")
+    if [[ -f "${PACKAGE}.inc" ]]; then
+        VERSION="$(sed 's/^#define __VERSION__ \(.*\)$/\1/p;d' "${PACKAGE}".inc)"
+    else
+        VERSION="UNKNOWN_VERSION"
+    fi
 
-        INC_LINES=0
-        if [[ -f "${CURRENT_DIR}/${BASENAME}.inc" ]]; then
+    if [[ -d .git ]]; then
+        BUILD_INFO="$(git branch | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/;s/\s\+/_/g')-$(git rev-parse --short HEAD)$([ -z "$(git status --porcelain 2>/dev/null | tail -n1)" ] || echo '-dirty')"
+        VERSION_STRING="${VERSION}-${BUILD_INFO}"
+    else
+        BUILD_INFO=
+        VERSION_STRING="${VERSION}"
+    fi
+    BUILD_DATE="$(date +"%a %d %b %Y %H:%M:%S %Z")"
+}
 
+banner() {
+    if [[ ${RELEASE_BUILD} -eq 0 ]]; then
+        local build_type="DEBUG"
+    else
+        local build_type="RELEASE"
+    fi
+    msg "${GREEN}===============|==============================${NOFORMAT}"
+    msg "${GREEN}       Package | ${NOFORMAT}${PACKAGE}"
+    msg "${GREEN}    Build Type | ${NOFORMAT}${build_type}"
+    msg "${GREEN}===============|==============================${NOFORMAT}"
+    msg "${GREEN}       Version | ${NOFORMAT}${VERSION}"
+    msg "${GREEN}    Build Info | ${NOFORMAT}${BUILD_INFO}"
+    msg "${GREEN}Version String | ${NOFORMAT}${VERSION_STRING}"
+    msg "${GREEN}    Build Date | ${NOFORMAT}${BUILD_DATE}"
+    msg "${GREEN}===============|==============================${NOFORMAT}"
+}
+
+prepare() {
+    msg "${GREEN}>>> ${NOFORMAT}Preparing..."
+
+    if [[ -n "$(lsof +D "${BASE_DIR}"/Classes)" ]]; then
+        die "Classes directory tree open in some program(s). Please close before continuing."
+    fi
+
+    if [[ -d "${BASE_DIR}"/.Classes  ]]; then
+        msg "${RED}(!) Original Classes source tree appears to have not been restored.${NOFORMAT}"
+        msg "${RED}    If you think this is in error, run 'make clean' before invoking the build script again.${NOFORMAT}"
+        msg "${ORANGE}Continuing in 5 seconds...${NOFORMAT}"
+        sleep 5
+        HASH="$(find "${BASE_DIR}"/.Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
+    else
+        HASH="$(find "${BASE_DIR}"/Classes -type f -iname '*.uc' -print0 | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}')"
+    fi
+
+    local old_hash="$(cat "${BASE_DIR}"/Classes.sha1sum 2>/dev/null)"
+    if [[ -f "${BASE_DIR}"/Classes.sha1sum && "${old_hash}" == "${HASH}" ]]; then
+        die "Source directory unchanged since last build. Nothing to do."
+    fi
+}
+
+preprocess() {
+    [[ ${NO_PREPROCESS} -eq 0 ]] || return
+
+    msg "${GREEN}>>> ${NOFORMAT}Preprocessing..."
+    if [[ -f "${BASE_DIR}"/Classes/.preprocessed ]]; then
+        msg "${RED}(!) Classes source tree appears to have already been preprocessed. Skipping...${NOFORMAT}"
+        msg "${RED}    If you think this is in error, run 'make clean' before invoking the build script again.${NOFORMAT}"
+    else
+        cp -r "${BASE_DIR}"/Classes "${BASE_DIR}"/.Classes
+
+        local opts=()
+        [[ "${RELEASE_BUILD}" -eq 0 ]] && opts+=("-D__DEBUG__")
+
+        local num_inc_lines=0
+        if [[ -f "${BASE_DIR}/${PACKAGE}.inc" ]]; then
             # Test number of lines in output include file
             gpp \
                 -n -U "" "" "(" "," ")" "(" ")" "#" "" \
@@ -111,51 +172,76 @@ if [[ ${NO_PREPROCESS} -eq 0 ]]; then
                 -D__BUILDINFO__="${BUILD_INFO}" \
                 -D__VERSIONSTRING__="${VERSION_STRING}" \
                 -D__BUILDDATE__="${BUILD_DATE}" \
-                -D__FILE__="${BASENAME}.inc" \
-                -o "${CURRENT_DIR}/.${BASENAME}.inc" \
-                "${CURRENT_DIR}/${BASENAME}.inc"
+                -D__FILE__="${PACKAGE}.inc" \
+                -o "${BASE_DIR}/.${PACKAGE}.inc" \
+                "${BASE_DIR}/${PACKAGE}.inc"
 
-            INC_LINES=$(wc -l "${CURRENT_DIR}/.${BASENAME}.inc" | cut -d' ' -f1)
-            rm "${CURRENT_DIR}/.${BASENAME}.inc"
-
-            OPTS+=("--nostdinc")
-            OPTS+=("--include ${CURRENT_DIR}/${BASENAME}.inc")
+            num_inc_lines=$(wc -l "${BASE_DIR}/.${PACKAGE}.inc" | cut -d' ' -f1)
+            rm "${BASE_DIR}/.${PACKAGE}.inc"
+            opts+=("--nostdinc")
+            opts+=("--include ${BASE_DIR}/${PACKAGE}.inc")
         fi
 
-        for FILE in "${CURRENT_DIR}"/.Classes/*.uc; do
-
-            DEST="${CURRENT_DIR}/Classes/$(basename "${FILE}")"
-
+        for file in "${BASE_DIR}"/.Classes/*.uc; do
+            local dest="${BASE_DIR}/Classes/$(basename "${file}")"
             gpp \
                 -n -U "" "" "(" "," ")" "(" ")" "#" "" \
                 -M "\n#\w" "\n" " " " " "\n" "" "" \
                 +csss "/*" "*/" +csss "//" "\n" +csss "\\\n" "" \
                 +s "\"" "\"" "\\" +s "'" "'" "\\" \
-                ${OPTS[@]} \
+                ${opts[@]} \
                 -D__VERSION__="${VERSION}" \
                 -D__BUILDINFO__="${BUILD_INFO}" \
                 -D__VERSIONSTRING__="${VERSION_STRING}" \
                 -D__BUILDDATE__="${BUILD_DATE}" \
-                -D__FILE__="$(basename "${FILE}")" \
-                -o "${DEST}" \
-                "${FILE}"
+                -D__FILE__="$(basename "${file}")" \
+                -o "${dest}" \
+                "${file}"
 
             # Remove lines occupied by include file if we used one
-            if [[ ${INC_LINES} -ne 0 ]]; then
-                tail -n +$((${INC_LINES} + 1)) "${DEST}" >"${DEST}.1"
-                mv -f "${DEST}.1" "${DEST}"
+            if [[ ${num_inc_lines} -ne 0 ]]; then
+                tail -n +$((${num_inc_lines} + 1)) "${dest}" >"${dest}.1"
+                mv -f "${dest}.1" "${dest}"
             fi
         done
-        touch "${CURRENT_DIR}"/Classes/.preprocessed
+        touch "${BASE_DIR}"/Classes/.preprocessed
     fi
-fi
-echo Compiling...
-[[ -f "${SYSTEM_DIR}/${BASENAME}.u" ]] && mv -vf "${SYSTEM_DIR}/${BASENAME}.u" "${SYSTEM_DIR}/.${BASENAME}.u.bak"
-{
-    ${COMPILE} make ini=..\\"${BASENAME}"\\make.ini 2>/dev/null && echo ${SHA1SUM} > "${CURRENT_DIR}"/Classes.sha1sum
-} || {
-    mv -vf "${SYSTEM_DIR}/.${BASENAME}.u.bak" "${SYSTEM_DIR}/${BASENAME}.u"
-    EXIT_STATUS=1
 }
-restore
-exit $EXIT_STATUS
+
+compile() {
+    msg "${GREEN}>>> ${NOFORMAT}Compiling..."
+    [[ -f "${SYSTEM_DIR}/${PACKAGE}.u" ]] && mv -vf "${SYSTEM_DIR}/${PACKAGE}.u" "${SYSTEM_DIR}/.${PACKAGE}.u.bak"
+
+    {
+        ${COMPILE} make ini=..\\"${PACKAGE}"\\make.ini 2>/dev/null && echo "${HASH}" >"${BASE_DIR}"/Classes.sha1sum
+        return 0
+    } || {
+        mv -vf "${SYSTEM_DIR}/.${PACKAGE}.u.bak" "${SYSTEM_DIR}/${PACKAGE}.u"
+        return 1
+    }
+}
+
+restore() {
+    if [[ ${NO_RESTORE} -eq 0 ]]; then
+        rm -rf "${BASE_DIR}"/Classes
+        mv -vf "${BASE_DIR}"/.Classes "${BASE_DIR}"/Classes
+    fi
+}
+
+main() {
+    setup_colors
+    parse_args
+
+    setup
+
+    banner
+    prepare
+    preprocess
+
+    local err=0
+    compile || err=1
+    restore
+    exit ${err}
+}
+
+main
